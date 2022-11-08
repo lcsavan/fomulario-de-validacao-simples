eeccdc3fb12a01d40a21158cc1f1b2fa6960156e
lcsavan <lucassavansantos@gmail.com>
2022-11-08 15:59:50 -0300

    Formulário de Validação Simples

---
+++ obrigado.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/estilo.css" rel="stylesheet">
+    <title>Mensagem Enviada</title>
+</head>
+<body>
+<main>
+    <h1>MENSAGEM ENVIADA</h1>
+    <a type="submit" href="index.php">Enviar Novamente</a>
+</main>
+</body>
+</html>
